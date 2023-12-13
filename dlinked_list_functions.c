4e6b44dbf79df5a6a38ebf32e45b27323f97e2bf
Emmanuel <kcemmy03@gmail.com>
2023-12-13 16:54:11 +0100

    Created the file that handles the doubly linked list

---
+++ dlinked_list_functions.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * add_node - adds a new item to the beginning of list.
+ * @head: pointer to the first node.
+ * @n: the data element of the node.
+ * Return: the address of the new node.
+ */
+stack_t *add_node(stack_t **head, const int n)
+{
+	stack *new_node, *temp;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		return (NULL);
+	temp = *head;
+	new_node->n = n;
+	new_node->next = temp;
+	new_node->prev = NULL;
+	if (temp != NULL)
+		temp->prev = new_node;
+	*head = new_node;
+
+	return (new_node);
+}
